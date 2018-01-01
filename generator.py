d9b077850178cb7346aca696f50e6d156b6bde8b
Mateusz Windak <mateuszw1995@gmail.com>
2018-01-01 20:01:09 +0100

    Generator added

---
+++ generator.py
@@ -0,0 +1,25 @@
+import sklearn
+from sklearn.datasets import make_moons
+import csv
+
+positions, y = sklearn.datasets.make_moons(1000, noise=0.20)
+with open('positions.csv', 'wb') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',')
+    for item in positions[0:700]:
+        print str(item)
+        spamwriter.writerow([str(item[0]) + " ", item[1]])
+with open('przynaleznosc.csv', 'wb') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',')
+    for item in y[0:700]:
+        print str(item)
+        spamwriter.writerow([item])
+with open('test.csv', 'wb') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',')
+    for item in positions[700:1000]:
+        print str(item)
+        spamwriter.writerow([str(item[0]) + " ", item[1]])
+with open('przynaleznosc_test.csv', 'wb') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',')
+    for item in y[700:1000]:
+        print str(item)
+        spamwriter.writerow([item])
